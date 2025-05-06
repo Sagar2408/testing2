1572b3e9f32225a506bdda794e039cee719b90ad
Sagar <sagar90160@gmail.com>
2025-05-06 11:59:20 +0530

    ok

--- sockets/screenSocket.js
+++ sockets/screenSocket.js
@@ -7,6 +7,14 @@ function screenSocket(socket, io) {
     socket.to(room).emit("screen-data", data);
   });
 
+  socket.on("audio-data", ({ room, data }) => {
+    socket.to(room).emit("audio-data", data);
+  });
+
+  socket.on("video-data", ({ room, data }) => {
+    socket.to(room).emit("video-data", data);
+  });
+
   socket.on("leave-room", (room) => {
     socket.leave(room);
   });
